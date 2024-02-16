a82cbbef214e909d52bc5697ccaafc39b90ed21e
Ignacio Speicys <ignacio.speicys@rappi.com>
2024-02-16 05:58:53 -0300

    PULUMI_CONFIG_PASSPHRASE placeholder commit

--- demo-pulumi-aws/infrastructure/setup/pulumiSetup.go
+++ demo-pulumi-aws/infrastructure/setup/pulumiSetup.go
@@ -22,31 +22,20 @@ func (ps *PulumiSetup) CreateWorkspace() {
 	ctx := context.Background()
 
 	proj := auto.Project(workspace.Project{
-		Name:    "demo-aws-pulumi",
+		Name:    "demo-pulumi-aws",
 		Runtime: workspace.NewProjectRuntimeInfo("go", nil),
 		Backend: &workspace.ProjectBackend{
 			URL: "file:///Users/ignaciospeicys/pulumi-file-backend/",
 		},
 	})
 
-	// File backend
-	localBackendURL := "file:///Users/ignaciospeicys/pulumi-file-backend/"
-	envVars := auto.EnvVars(map[string]string{
-		"PULUMI_BACKEND_URL":       localBackendURL,
-		"PULUMI_CONFIG_PASSPHRASE": "n4ch0Pu1um1",
-	})
-
-	// Creating a new local workspace with these environment variables
-	workspaceOptions := []auto.LocalWorkspaceOption{
-		proj,
-		envVars,
-	}
-
-	w, err := auto.NewLocalWorkspace(ctx, workspaceOptions...)
+	w, err := auto.NewLocalWorkspace(ctx, proj)
 	if err != nil {
 		fmt.Printf("Failed to initialize local workspace: %v\n", err)
 		os.Exit(1)
 	}
+	w.SetEnvVar("PULUMI_CONFIG_PASSPHRASE", "n4ch0Pu1um1")
+
 	err = w.InstallPlugin(ctx, "aws", awsPluginVersion)
 	if err != nil {
 		fmt.Printf("Failed to install program plugins: %v\n", err)
